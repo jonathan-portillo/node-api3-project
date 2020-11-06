fb461fd4216fe1bb0fe7c3b5efa2cca25c837a12
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2020-11-06 00:14:13 -0500

    .env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const server = require("./server");
 
-const port = 5000;
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log(`\n Server running on localhost:${port} \n`);
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const helmet = require("helmet");
 const userRouter = require("./users/userRouter.js");
 const postRouter = require("./posts/postRouter.js");
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
 server.use("/user", userRouter);
 server.use("/posts", postRouter);
 server.use(logger);
